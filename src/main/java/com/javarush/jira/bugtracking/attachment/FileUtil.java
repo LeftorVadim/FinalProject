3633672bc9623ae90e696d5e1f9d8ceda00d4997
Leftor Vadim <leftorvadim@gmail.com>
2023-12-27 16:52:35 +0500

    The method com.javarush.jira.bugtracking.attachment.FileUtil#upload has been refactored

--- src/main/java/com/javarush/jira/bugtracking/attachment/FileUtil.java
+++ src/main/java/com/javarush/jira/bugtracking/attachment/FileUtil.java
@@ -33,8 +33,8 @@ public class FileUtil {
                     throw new IllegalArgumentException("Failed to upload file " + multipartFile.getOriginalFilename());
                 }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 
